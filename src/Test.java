7e31e97aa777e2c1bccfe7eab1ee902f29c29470
ml <2053151689@qq.com>
2020-11-28 23:08:59 +0800

    这是我的第一次提交

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+/**
+ * @Auther: coding
+ * @Date: 2020/11/28 22:53
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Git测试");
+    }
+}
